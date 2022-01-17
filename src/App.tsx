e4da19d6dd8ad4a3e71b7eb0f085299f93de30db
vojtech.holan <vojtech.holan@3pillarglobal.com>
2022-01-17 19:29:29 +0100

    feat: prepare landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <img className={styles.logo} src={`${process.env.PUBLIC_URL}/logo.png`} alt="3Pillar Global logo"/>
+      <h1>Documentation</h1>
+
+      <div className={styles.boxes}>
+        <section>
+          <h2>Official websites</h2>
+          <p>3Pillar Global builds breakthrough software products that power digital businesses. Trusted services.
+            Tested results. Innovations that scale.</p>
+          <a
+            href="https://www.3pillarglobal.com/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            More {'>'}
+          </a>
+        </section>
+        <section>
+          <h2>Github - React starting repository</h2>
+          <p>This repository helps you to start programming faster and more effective!</p>
+          <a
+            href="https://github.com/3PillarGlobal-Ostrava/interview-app-ui"
+            target="_blank"
+            rel="noreferrer"
+          >More {'>'}</a>
+        </section>
+      </div>
     </div>
   );
 }
