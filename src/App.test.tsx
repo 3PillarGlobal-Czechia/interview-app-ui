1f63d68565abb1a6a996848ebe8c8380a16bbaa2
vojtech.holan <vojtech.holan@3pillarglobal.com>
2022-01-23 11:39:00 +0100

    feat: Added some tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,8 +3,44 @@ import React from 'react';
 
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders particular section', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const threePillarSection = screen.getByTestId('3pillar-global-section');
+  const githubSection = screen.getByTestId(
+    'github-starting-repository-section'
+  );
+
+  expect(threePillarSection).toBeInTheDocument();
+  expect(githubSection).toBeInTheDocument();
+});
+
+test('3pillar section has right title', () => {
+  render(<App />);
+  const threePillarSection = screen.getByTestId('3pillar-global-section');
+
+  expect(threePillarSection.querySelector('h2')).toHaveTextContent(
+    'Official websites'
+  );
+});
+
+test('3pillar section has right description', () => {
+  render(<App />);
+  const threePillarSection = screen.getByTestId('3pillar-global-section');
+
+  expect(threePillarSection.querySelector('p')).toHaveTextContent(
+    `3Pillar Global builds breakthrough software products that power digital businesses. Trusted services. Tested results. Innovations that scale.`
+  );
+});
+
+test('3pillar section has right link', () => {
+  render(<App />);
+  const threePillarSection = screen.getByTestId('3pillar-global-section');
+  const link = threePillarSection.querySelector('button');
+
+  expect(link).toHaveTextContent(`More >`);
+
+  window.open = jest.fn();
+  link?.click();
+
+  expect(window.open).toHaveBeenCalled();
 });
--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,23 @@ import React from 'react';
 import styles from './App.module.scss';
 
 function App(): JSX.Element {
+  const handle3pillarGlobalButtonClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ): void => {
+    e.preventDefault();
+    window.open('https://www.3pillarglobal.com/', '_blank');
+  };
+
+  const handleGithubButtonClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ): void => {
+    e.preventDefault();
+    window.open(
+      'https://github.com/3PillarGlobal-Ostrava/interview-app-ui',
+      '_blank'
+    );
+  };
+
   return (
     <div className={styles.app}>
       <img
@@ -13,35 +30,26 @@ function App(): JSX.Element {
       <h1>Documentation</h1>
 
       <div className={styles.boxes}>
-        <section>
+        <section data-testid="3pillar-global-section">
           <h2>Official websites</h2>
           <p>
             3Pillar Global builds breakthrough software products that power
             digital businesses. Trusted services. Tested results. Innovations
             that scale.
           </p>
-          <a
-            href="https://www.3pillarglobal.com/"
-            rel="noreferrer"
-            target="_blank"
-          >
+          <button type="button" onClick={handle3pillarGlobalButtonClick}>
             More {'>'}
-          </a>
+          </button>
         </section>
-        <section>
+        <section data-testid="github-starting-repository-section">
           <h2>Github - React starting repository</h2>
           <p>
             This repository helps you to start programming faster and more
             effective!
           </p>
-          <a
-            href="https://github.com/3PillarGlobal-Ostrava/interview-app-ui"
-            rel="noreferrer"
-            target="_blank"
-          >
-            More
-            {'>'}
-          </a>
+          <button type="button" onClick={handleGithubButtonClick}>
+            More {'>'}
+          </button>
         </section>
       </div>
     </div>
