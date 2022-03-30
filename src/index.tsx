a5ea296854e3d87cd12edbb4cc8b283bd1068549
robertstriz <100909070+robertstriz@users.noreply.github.com>
2022-03-30 16:32:35 +0200

    add application insight support (#31)
    * add application insight support
    add examples
    
    * fix typo

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,59 @@
 import './index.module.scss';
 import 'antd/dist/antd.css';
 
+import {
+  AppInsightsContext,
+  ReactPlugin,
+  withAITracking,
+} from '@microsoft/applicationinsights-react-js';
+import {
+  ApplicationInsights,
+  IExceptionTelemetry,
+} from '@microsoft/applicationinsights-web';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { createBrowserHistory } from 'history';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// #region Application insight
+const browserHistory = createBrowserHistory({});
+const reactPlugin = new ReactPlugin();
+export default withAITracking(reactPlugin, App);
+export const appInsights = new ApplicationInsights({
+  config: {
+    instrumentationKey: process.env.REACT_APP_APPINSIGHTS_KEY,
+    enableAutoRouteTracking: true,
+    extensions: [reactPlugin],
+    extensionConfig: {
+      [reactPlugin.identifier]: { history: browserHistory },
+    },
+  },
+});
+
+appInsights.loadAppInsights();
+
+window.addEventListener('unhandledrejection', (event) => {
+  // eslint-disable-next-line no-console
+  console.log(event);
+  appInsights.trackException({ error: event.reason } as IExceptionTelemetry);
+});
+// #endregion
+
+try {
+  ReactDOM.render(
+    <React.StrictMode>
+      <AppInsightsContext.Provider value={reactPlugin}>
+        <App />
+      </AppInsightsContext.Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+} catch (err: unknown) {
+  appInsights.trackException({ error: err } as IExceptionTelemetry);
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
