78fbb52240247ce06d9edd3228cfddbb34ca8c30
Tomáš Niemczyk <tomas.niemczyk@3pillarglobal.com>
2022-03-30 18:57:52 +0200

    add header for interview page with the ability to quit interview

--- src/pages/InterviewPage/Interview.tsx
+++ src/pages/InterviewPage/Interview.tsx
@@ -1,7 +1,8 @@
-import { List } from 'antd';
+import { List, Modal } from 'antd';
 import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 
+import InterviewHeader from '../../domain/Interview/InterviewHeader';
 import QuestionView from '../../domain/Interview/QuestionView';
 import ScalableBody from '../../layout/scalableBody/ScalableBody';
 import {
@@ -13,8 +14,10 @@ import {
 export default function Interview(): JSX.Element {
   const { id } = useParams<'id'>();
   const client = new Client();
+  const navigate = useNavigate();
   const [questionSetDetail, setQuestionSetDetail] =
     useState<QuestionSetDetail>();
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
 
   useEffect(() => {
     client
@@ -24,15 +27,30 @@ export default function Interview(): JSX.Element {
 
   return (
     <ScalableBody>
-      <List<QuestionModel>
-        itemLayout="horizontal"
-        dataSource={questionSetDetail?.questions}
-        renderItem={(question: QuestionModel) => (
-          <List.Item>
-            <QuestionView question={question} />
-          </List.Item>
-        )}
-      />
+      <div>
+        <Modal
+          visible={isModalVisible}
+          title="Quit interview?"
+          onOk={() => navigate('/')}
+          onCancel={() => setIsModalVisible(false)}
+          okButtonProps={{ danger: true }}
+        >
+          <p>Any data entered will be lost!</p>
+        </Modal>
+        <InterviewHeader
+          questionSetName={questionSetDetail?.questionSet?.title ?? ''}
+          buttonClickedCallback={() => setIsModalVisible(true)}
+        />
+        <List<QuestionModel>
+          itemLayout="horizontal"
+          dataSource={questionSetDetail?.questions}
+          renderItem={(question: QuestionModel) => (
+            <List.Item>
+              <QuestionView question={question} />
+            </List.Item>
+          )}
+        />
+      </div>
     </ScalableBody>
   );
 }
