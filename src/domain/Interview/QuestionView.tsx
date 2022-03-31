dbe73fc1d04f0b9a64b0f908d7bca3ab1aee1bd5
Tomáš Niemczyk <tomas.niemczyk@3pillarglobal.com>
2022-03-31 13:42:13 +0200

    remove answer field

--- src/domain/Interview/QuestionView.tsx
+++ src/domain/Interview/QuestionView.tsx
@@ -1,18 +1,14 @@
-import { Input, Rate } from 'antd';
-import React, { useState } from 'react';
+import { Rate } from 'antd';
+import React from 'react';
 
 import { QuestionModel } from '../../services/Client';
 import styles from './QuestionView.module.scss';
-
-const { TextArea } = Input;
 
 export default function InterviewQuestionView({
   question,
 }: {
   question: QuestionModel;
 }): JSX.Element {
-  const [answer, setAnswer] = useState<string>();
-
   return (
     <div className={styles.questionView}>
       <div>
@@ -23,12 +19,6 @@ export default function InterviewQuestionView({
         </div>
         <p>{question.content}</p>
       </div>
-      <TextArea
-        placeholder="Answer"
-        value={answer}
-        onChange={(e) => setAnswer(e.target.value)}
-        rows={4}
-      />
     </div>
   );
 }
