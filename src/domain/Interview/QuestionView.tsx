d599288067c7694da80b8ef91652259cf4d61abd
Tomáš Niemczyk <tomas.niemczyk@3pillarglobal.com>
2022-03-31 13:53:35 +0200

    add option to rate answer

--- src/domain/Interview/QuestionView.tsx
+++ src/domain/Interview/QuestionView.tsx
@@ -1,5 +1,5 @@
-import { Rate } from 'antd';
-import React from 'react';
+import { InputNumber, Rate } from 'antd';
+import React, { useState } from 'react';
 
 import { QuestionModel } from '../../services/Client';
 import styles from './QuestionView.module.scss';
@@ -9,6 +9,8 @@ export default function InterviewQuestionView({
 }: {
   question: QuestionModel;
 }): JSX.Element {
+  const [value, setValue] = useState<number>();
+
   return (
     <div className={styles.questionView}>
       <div>
@@ -17,7 +19,13 @@ export default function InterviewQuestionView({
           <p>{question.category}</p>
           <Rate disabled value={question.difficulty} />
         </div>
-        <p>{question.content}</p>
+        <div>
+          <span>{question.content}</span>
+          <div>
+            <span>Rate answer (1 - 10)</span>
+            <InputNumber min={1} max={10} value={value} onChange={setValue} />
+          </div>
+        </div>
       </div>
     </div>
   );
