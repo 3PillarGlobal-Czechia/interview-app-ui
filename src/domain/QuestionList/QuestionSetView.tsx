e58a70457cf26a4de1ac8d66bd72cdf84df805b7
Tomáš Niemczyk <tomas.niemczyk@3pillarglobal.com>
2022-04-04 15:52:17 +0200

    add components to view

--- src/domain/QuestionList/QuestionSetView.tsx
+++ src/domain/QuestionList/QuestionSetView.tsx
@@ -3,6 +3,8 @@ import { Spin } from 'antd';
 import React from 'react';
 
 import { QuestionSetDetail } from '../../services/Client';
+import AverageDifficultyCircle from './AverageDifficultyCircle';
+import DistinctCategoryTags from './DistinctCategoryTags';
 import QuestionSetViewHeader from './QuestionSetViewHeader';
 
 export default function QuestionSetView({
@@ -14,7 +16,17 @@ export default function QuestionSetView({
     return <Spin indicator={<LoadingOutlined />} />;
   }
 
-  return <>
-    <QuestionSetViewHeader title={list?.questionSet?.title ?? ''} />
-  </>;
+  return (
+    <>
+      <QuestionSetViewHeader title={list?.questionSet?.title ?? ''} />
+      <>
+        <AverageDifficultyCircle />
+        <p>
+          This set contains {list.questions?.length} questions with the
+          following tags:
+        </p>
+        <DistinctCategoryTags />
+      </>
+    </>
+  );
 }
