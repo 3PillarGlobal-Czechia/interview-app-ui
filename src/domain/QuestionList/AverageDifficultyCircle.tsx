0c21d9d6e488b7bfe5728904f64d069d557a009b
Tomáš Niemczyk <tomas.niemczyk@3pillarglobal.com>
2022-04-04 15:48:07 +0200

    move average difficulty circle to custom component

---
+++ src/domain/QuestionList/AverageDifficultyCircle.tsx
@@ -0,0 +1,28 @@
+import { Progress } from 'antd';
+import React from 'react';
+
+import { difficultyPercentageToColor } from '../../services/mathUtils';
+import AverageDifficultyContent from './AverageDifficultyContent';
+
+export default function AverageDifficultyCircle({
+  percent,
+}: {
+  percent?: number;
+}): JSX.Element {
+  const averageDifficultyContentElement = (
+    percentage?: number
+  ): JSX.Element => <AverageDifficultyContent percent={percentage} />;
+
+  return (
+    <Progress
+      type="circle"
+      format={averageDifficultyContentElement}
+      percent={percent}
+      strokeColor={difficultyPercentageToColor(percent ?? 0)}
+    />
+  );
+}
+
+AverageDifficultyCircle.defaultProps = {
+  percent: undefined,
+};
