767cdfb3f5c56f7b8b0003e23bf5f2b1f96badd9
Tomáš Niemczyk <tomas.niemczyk@3pillarglobal.com>
2022-04-04 19:44:51 +0200

    add question set view header styling

--- src/domain/QuestionList/QuestionSetViewHeader.tsx
+++ src/domain/QuestionList/QuestionSetViewHeader.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Header from '../../layout/header/Header';
+import styles from './QuestionSetView.module.scss';
 
 export default function QuestionSetViewHeader({
   title,
@@ -12,13 +13,13 @@ export default function QuestionSetViewHeader({
   return (
     <Header
       left={
-        <>
+        <div className={styles.questionSetViewHeader}>
           <Link to="/" className="text-black">
             <ArrowLeftOutlined />
           </Link>
           <h3>{title}</h3>
           <EditOutlined />
-        </>
+        </div>
       }
       right={<span>By anonymous</span>}
     />
