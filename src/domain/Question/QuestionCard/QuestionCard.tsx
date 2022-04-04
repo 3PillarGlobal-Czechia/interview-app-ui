3205d2cea90f2b2851ac5b9e2ff853ccba915f2a
Tomáš Niemczyk <tomas.niemczyk@3pillarglobal.com>
2022-04-04 21:17:58 +0200

    add border left color to question card

--- src/domain/Question/QuestionCard/QuestionCard.tsx
+++ src/domain/Question/QuestionCard/QuestionCard.tsx
@@ -5,6 +5,8 @@ import React from 'react';
 import { QuestionModel } from '../../../services/Client';
 import styles from './QuestionCard.module.scss';
 
+import { difficultyPercentageToColor } from '../../../services/mathUtils';
+
 export default function QuestionCard({
   question,
   tagColor,
@@ -14,13 +16,15 @@ export default function QuestionCard({
   tagColor: string;
   deleteClickedCallback: (id: number) => void;
 }): JSX.Element {
+  const color = question.difficulty ? difficultyPercentageToColor(question.difficulty) : '#FFF';
+
   return (
-    <div className={styles.questionCard}>
+    <div className={styles.questionCard} style={{ borderLeftColor: color }}>
       <span>{question.title}</span>
       <div>
         <Tag color={tagColor}>{question.category}</Tag>
         <DeleteOutlined onClick={() => question.id ? deleteClickedCallback(question.id) : null} style={{ color: 'red' }} />
       </div>
-    </div>
+    </div >
   );
 }
