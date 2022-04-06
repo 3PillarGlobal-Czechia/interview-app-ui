abd51047923e862de2f8d65b2a439c1dbd284984
Tomáš Niemczyk <tomas.niemczyk@3pillarglobal.com>
2022-04-06 09:54:11 +0200

    add questions to antd list

--- src/domain/QuestionList/AvailableQuestionsView.tsx
+++ src/domain/QuestionList/AvailableQuestionsView.tsx
@@ -1,12 +1,18 @@
-import { Input, Select } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
+import { Input, List, Select, Spin } from 'antd';
 import React from 'react';
 import { QuestionModel } from '../../services/Client';
+import QuestionCardLarge from '../Question/QuestionCard/QuestionCardLarge';
 
 import styles from './QuestionSetView.module.scss';
 
 const { Search } = Input;
 
 export default function AvailableQuestionsView({ availableQuestions }: { availableQuestions: QuestionModel[] | undefined }): JSX.Element {
+    const questionListElement = availableQuestions
+        ? <List dataSource={availableQuestions} renderItem={item => <QuestionCardLarge question={item} />} />
+        : <Spin indicator={<LoadingOutlined />} />
+
     return (
         <div className={styles.availableQuestions}>
             <h3>Available Questions</h3>
@@ -15,6 +21,7 @@ export default function AvailableQuestionsView({ availableQuestions }: { availab
                 <Select placeholder='Category' />
                 <Select placeholder='Difficulty' />
             </div>
+            {questionListElement}
         </div>
     );
 }
