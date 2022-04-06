c0ca04eed53135b48ed66dd85b7e00ea70bad71e
Tomáš Niemczyk <tomas.niemczyk@3pillarglobal.com>
2022-04-06 10:27:20 +0200

    add card content and styles

--- src/domain/Question/QuestionCard/QuestionCardLarge.tsx
+++ src/domain/Question/QuestionCard/QuestionCardLarge.tsx
@@ -1,9 +1,21 @@
+import { CheckCircleOutlined, MoreOutlined } from '@ant-design/icons';
+import { Dropdown } from 'antd';
 import React from 'react';
 import { QuestionModel } from '../../../services/Client';
 
+import styles from './QuestionCard.module.scss';
+
 export default function QuestionCardLarge({ question }: { question: QuestionModel }): JSX.Element {
     return (
-        <>
-        </>
+        <div className={styles.questionCardLarge}>
+            <CheckCircleOutlined style={{ fontSize: '32px' }} />
+            <span>{question.title}</span>
+            <div>
+                <Dropdown overlay={<p>What to place here?</p>} placement="topLeft">
+                    <MoreOutlined onClick={(e) => e.stopPropagation()} />
+                </Dropdown>
+                <span>{question.category}</span>
+            </div>
+        </div>
     );
 }
