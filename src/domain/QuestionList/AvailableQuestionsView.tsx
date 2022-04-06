c8801b90d4193deaab1bd44127436154541cc131
Tomáš Niemczyk <tomas.niemczyk@3pillarglobal.com>
2022-04-06 12:55:28 +0200

    add category filtering

--- src/domain/QuestionList/AvailableQuestionsView.tsx
+++ src/domain/QuestionList/AvailableQuestionsView.tsx
@@ -1,6 +1,7 @@
 import { LoadingOutlined } from '@ant-design/icons';
 import { Input, List, Select, Spin } from 'antd';
 import React, { useState } from 'react';
+import { arrayDistinct } from '../../services/arrayUtils';
 import { QuestionModel } from '../../services/Client';
 import { filterQuestions } from '../../services/filterService';
 import QuestionCardLarge from '../Question/QuestionCard/QuestionCardLarge';
@@ -11,9 +12,10 @@ const { Search } = Input;
 
 export default function AvailableQuestionsView({ availableQuestions, questionsAddedToSet, addToSetCallback }: { availableQuestions: QuestionModel[] | undefined; questionsAddedToSet: QuestionModel[] | undefined; addToSetCallback: (question: QuestionModel) => void; }): JSX.Element {
     const [searchText, setSearchText] = useState('');
+    const [searchCategories, setSearchCategories] = useState<string[]>();
 
     const questionListElement = availableQuestions && questionsAddedToSet
-        ? <List dataSource={filterQuestions(availableQuestions, searchText, undefined, undefined, undefined)} renderItem={item => <QuestionCardLarge question={item} addToSetCallback={addToSetCallback} isContainedInSet={questionsAddedToSet.map(q => q.id).includes(item.id)} />} />
+        ? <List dataSource={filterQuestions(availableQuestions, searchText, searchCategories, undefined, undefined)} renderItem={item => <QuestionCardLarge question={item} addToSetCallback={addToSetCallback} isContainedInSet={questionsAddedToSet.map(q => q.id).includes(item.id)} />} />
         : <Spin indicator={<LoadingOutlined />} />
 
     return (
@@ -21,8 +23,8 @@ export default function AvailableQuestionsView({ availableQuestions, questionsAd
             <h3>Available Questions</h3>
             <div>
                 <Search placeholder='Search available questions' allowClear value={searchText} onChange={(e) => setSearchText(e.target.value)} />
-                <Select placeholder='Category'>
-                    {availableQuestions?.map(question => <Select.Option value={question.category}>{question.category}</Select.Option>)}
+                <Select placeholder='Category' mode='multiple' value={searchCategories} onChange={setSearchCategories}>
+                    {availableQuestions && arrayDistinct(availableQuestions.map(question => question.category)).map(category => <Select.Option value={category}>{category}</Select.Option>)}
                 </Select>
                 <Select placeholder='Difficulty' />
             </div>
