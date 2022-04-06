bca0b581c1387da10300fd2d3783b0b31d3732fc
Tomáš Niemczyk <tomas.niemczyk@3pillarglobal.com>
2022-04-06 11:12:14 +0200

    add tooltip to checked icon

--- src/domain/Question/QuestionCard/AddIcon.tsx
+++ src/domain/Question/QuestionCard/AddIcon.tsx
@@ -1,12 +1,18 @@
 import { CheckCircleOutlined, PlusCircleOutlined } from '@ant-design/icons';
+import { Tooltip } from 'antd';
 import React from 'react';
 
 import styles from './QuestionCard.module.scss';
 
 export default function AddIcon({ isAddIcon }: { isAddIcon: boolean }): JSX.Element {
+    const tooltip = 'This question is already in this set';
+
     return (
         <>
-            {isAddIcon ? <PlusCircleOutlined className={styles.icon} /> : <CheckCircleOutlined className={styles.icon} />}
+            {isAddIcon ? <PlusCircleOutlined className={styles.icon} /> :
+                <Tooltip title={tooltip} placement='right'>
+                    <CheckCircleOutlined className={styles.icon} />
+                </Tooltip>}
         </>
     );
 }
