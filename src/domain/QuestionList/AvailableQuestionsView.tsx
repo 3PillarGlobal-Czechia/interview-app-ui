1315c7f0d6afa69afc5941312ace3c439ae2816c
Tomáš Niemczyk <tomas.niemczyk@3pillarglobal.com>
2022-04-06 09:40:49 +0200

    add basic search inputs to available questions

--- src/domain/QuestionList/AvailableQuestionsView.tsx
+++ src/domain/QuestionList/AvailableQuestionsView.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import { Input, Select } from 'antd';
+import React, { useState } from 'react';
+
+import styles from './QuestionSetView.module.scss';
 
 export default function AvailableQuestionsView(): JSX.Element {
+    const [inputValue, setInputValue] = useState('');
+
     return (
-        <>
-        </>
+        <div className={styles.availableQuestions}>
+            <h3>Available Questions</h3>
+            <div>
+                <Input value={inputValue} onChange={e => setInputValue(e.target.value)} placeholder='Search available questions' />
+                <Select placeholder='Category' />
+                <Select placeholder='Difficulty' />
+            </div>
+        </div>
     );
 }
