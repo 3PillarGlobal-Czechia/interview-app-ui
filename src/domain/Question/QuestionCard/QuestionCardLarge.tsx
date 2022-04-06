cf14bf3a16f533556f80b5b86fb09c33b57866f7
Tomáš Niemczyk <tomas.niemczyk@3pillarglobal.com>
2022-04-06 09:53:28 +0200

    add a card for addable questions

---
+++ src/domain/Question/QuestionCard/QuestionCardLarge.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { QuestionModel } from '../../../services/Client';
+
+export default function QuestionCardLarge({ question }: { question: QuestionModel }): JSX.Element {
+    return (
+        <>
+        </>
+    );
+}
