91f0c2bb97282d26ba55736928a208dce9695dc7
Tomáš Niemczyk <tomas.niemczyk@3pillarglobal.com>
2022-04-06 13:44:29 +0200

    change category tag color to be a hash of it's category name

--- src/services/tagCategoryColorService.ts
+++ src/services/tagCategoryColorService.ts
@@ -1,4 +1,5 @@
 import { QuestionModel } from './Client';
+import { hashCode } from './stringUtils';
 
 const colors: string[] = [
   'red',
@@ -9,20 +10,9 @@ const colors: string[] = [
   'yellow',
   'black',
 ];
-let colorIndex = 0;
-const hashmap = new Map<string, string>();
-
-const nextColor = (): string => {
-  const color = colors[colorIndex];
-  colorIndex = (colorIndex + 1) % colors.length;
-  return color;
-};
 
 export function colorByCategory(category: string): string {
-  if (!hashmap.has(category)) {
-    hashmap.set(category, nextColor());
-  }
-  return hashmap.get(category) ?? 'black';
+  return colors[Math.abs(hashCode(category)) % colors.length];
 }
 
 export function getDistinctCategories(questions: QuestionModel[]): string[] {
