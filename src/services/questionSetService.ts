a3f6a9c79a81485b02ce3ada4054fc288d280587
Tomáš Niemczyk <tomas.niemczyk@3pillarglobal.com>
2022-04-11 13:57:01 +0200

    add service for calculating average difficulty

---
+++ src/services/questionSetService.ts
@@ -0,0 +1,10 @@
+import { average } from '../helper/mathUtils';
+import { QuestionModel } from './Client';
+
+export function averageDifficulty(questions: QuestionModel[]): number {
+  return average(
+    questions
+      .filter((q) => q.difficulty !== undefined)
+      .map((q) => q.difficulty!) // eslint-disable-line @typescript-eslint/no-non-null-assertion
+  ) * 20;
+}
